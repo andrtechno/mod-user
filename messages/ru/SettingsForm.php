599096a166ff7ac0010764c23775037328c6aaf3
Andrew <andrew.panix@gmail.com>
2019-03-20 20:31:01 +0200

    init

--- messages/ru/SettingsForm.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'LOGIN_DURATION'=>'Login duration',
-    'ENABLE_REGISTER'=>'Активировать регистрацию новых пользователей',
-    'ENABLE_FORGOT'=>'Активировать возращение паролей',
-    'ENABLE_SOCIAL_AUTH'=>'Активировать авторизацию через социальные сети',
-
-];
