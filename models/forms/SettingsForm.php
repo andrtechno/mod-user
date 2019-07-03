a0abb1fd9da3f0c4e211dad700f1a8fd9fe1f694
Andrew <andrew.panix@gmail.com>
2019-07-03 16:28:15 +0300

    init

--- models/forms/SettingsForm.php
+++ models/forms/SettingsForm.php
@@ -2,9 +2,12 @@
 
 namespace panix\mod\user\models\forms;
 
+use Yii;
 use panix\engine\SettingsModel;
+use yii\helpers\ArrayHelper;
 
-class SettingsForm extends SettingsModel {
+class SettingsForm extends SettingsModel
+{
 
     public static $category = 'user';
     protected $module = 'user';
@@ -38,15 +41,47 @@ class SettingsForm extends SettingsModel {
     public $oauth_live_id;
     public $oauth_live_secret;
 
-    public function rules() {
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->login_duration = $this->login_duration / 86400;
+        parent::init();
+
+
+        $list = [];
+
+     //   echo $this->login_duration / 86400;die;
+       // $param = Yii::$app->settings->get(static::$category);
+        //$list['login_duration'] = $param->login_duration / 86400;
+
+        //$this->attributes = ArrayHelper::merge((array) $param, $list);
+        //$this->setAttributes(ArrayHelper::merge((array) $param, $list));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function save()
+    {
+        $this->login_duration = $this->login_duration * 86400;
+        parent::save();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
         return [
             [['login_duration'], "required"],
             [['login_duration'], 'integer'],
-            [['enable_register','enable_forgot','enable_social_auth'], 'boolean'],
-            [['mail_forgot'],'string'],
+            [['enable_register', 'enable_forgot', 'enable_social_auth'], 'boolean'],
+            [['mail_forgot'], 'string'],
 
-            [['oauth_facebook_id','oauth_google_id','oauth_vkontakte_id','oauth_github_id','oauth_yandex_id','oauth_twitter_id','oauth_linkedin_id','oauth_live_id'],'string'],
-            [['oauth_facebook_secret','oauth_google_secret','oauth_vkontakte_secret','oauth_github_secret','oauth_yandex_secret','oauth_twitter_secret','oauth_linkedin_secret','oauth_live_secret'],'string'],
+            [['oauth_facebook_id', 'oauth_google_id', 'oauth_vkontakte_id', 'oauth_github_id', 'oauth_yandex_id', 'oauth_twitter_id', 'oauth_linkedin_id', 'oauth_live_id'], 'string'],
+            [['oauth_facebook_secret', 'oauth_google_secret', 'oauth_vkontakte_secret', 'oauth_github_secret', 'oauth_yandex_secret', 'oauth_twitter_secret', 'oauth_linkedin_secret', 'oauth_live_secret'], 'string'],
         ];
     }
 
@@ -54,12 +89,13 @@ class SettingsForm extends SettingsModel {
      * Настройки по умолчанию
      * @return array
      */
-    public static function defaultSettings() {
+    public static function defaultSettings()
+    {
         return [
             'login_duration' => 2592000,
-            'enable_register'=>true,
-            'enable_forgot'=>true,
-            'enable_social_auth'=>true,
+            'enable_register' => true,
+            'enable_forgot' => true,
+            'enable_social_auth' => true,
         ];
     }
 
